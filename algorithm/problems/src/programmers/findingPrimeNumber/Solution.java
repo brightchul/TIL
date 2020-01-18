a6f26d36ca8cab04f8dfc8fb2acfd08f642de955
brightchul <abhbtbb@gmail.com>
2020-01-18 09:46:47 +0900

    [modify] 프로그래머스 소수찾기 코드 수정
    소수 아닌 것들은 이미 이전에 다 체크가 되어서 건너뛰도록 수정

--- algorithm/problems/src/programmers/findingPrimeNumber/Solution.java
+++ algorithm/problems/src/programmers/findingPrimeNumber/Solution.java
@@ -32,6 +32,7 @@ public class Solution {
         for(int i=2; i<arr.length; i++) arr[i] = 1;
 
         for(int i=2; i<=limit; i++) {
+            if(arr[i] == 0) continue;
             for(int idx = i*i; idx<=maxValue; idx+=i) {
                 arr[idx] = 0;
             }
@@ -39,18 +40,18 @@ public class Solution {
         return arr;
     }
     private void recursive(String numbers, String accNumStr) {
-        if(numbers.length() == 0) return;
+        if (numbers.length() == 0) return;
         int len = numbers.length();
-        for(int i=0; i<len; i++) {
+        for (int i = 0; i < len; i++) {
             char one = numbers.charAt(i);
-            String nextNumbers = numbers.substring(0, i) + numbers.substring(i+1);
+            String nextNumbers = numbers.substring(0, i) + numbers.substring(i + 1);
 
             String newAccNumStr1 = one + accNumStr;
             numSet.add(Integer.parseInt(newAccNumStr1));
             recursive(nextNumbers, newAccNumStr1);
 
             String newAccNumStr2 = accNumStr + one;
-            if(newAccNumStr1.equals(newAccNumStr2)) continue;
+            if (newAccNumStr1.equals(newAccNumStr2)) continue;
             numSet.add(Integer.parseInt(newAccNumStr2));
             recursive(nextNumbers, newAccNumStr2);
         }
--- algorithm/problems/src/programmers/findingPrimeNumber/Solution.md
+++ algorithm/problems/src/programmers/findingPrimeNumber/Solution.md
@@ -3,9 +3,8 @@
 ## 문제 설명 [[링크]](https://programmers.co.kr/learn/courses/30/lessons/42839)
 
 ```
-한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 
-만들 수 있는지 알아내려 합니다. 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 
-주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 알수 있는 함수를 만드세요.
+한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수
+있는지 알아내려 합니다. 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
 
 [제한사항]
@@ -65,6 +64,7 @@ private int[] makePrimeArr(int maxValue) {
     for(int i=2; i<arr.length; i++) arr[i] = 1;
 
     for(int i=2; i<=limit; i++) {
+        if(arr[i] == 0) continue;
         for(int idx = i*i; idx<=maxValue; idx+=i) {
             arr[idx] = 0;
         }
