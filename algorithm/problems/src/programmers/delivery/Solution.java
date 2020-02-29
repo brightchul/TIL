fa3cedc4418c31e5180bf76768e94bbea0fb0c35
brightchul <abhbtbb@gmail.com>
2020-02-29 22:54:26 +0900

    [add] 프로그래머스 배달 문제 1차 시도
    1차 시도로 그래프, BFS, 우선순위큐를 이용했으나 통과되질 못했다.

---
+++ algorithm/problems/src/programmers/delivery/Solution.java
@@ -0,0 +1,87 @@
+package programmers.delivery;
+
+import java.util.*;
+
+/*
+코딩테스트 연습 < 서머코딩/윈터코딩(~2018) < 배달
+https://programmers.co.kr/learn/courses/30/lessons/12978
+ */
+
+public class Solution {
+    public int solution(int N, int[][] road, int K) {
+        MyGraph mg = new MyGraph(N);
+        for(int[] one : road)
+            mg.add(one[0], one[1], one[2]);
+        int result = mg.countDelivery(K);
+        return result;
+    }
+}
+
+
+class MyGraph {
+    private PriorityQueue<AccNode> heap;
+    private List<HashMap<Integer, Integer>> list;
+    private int len;
+    private int timeLimit;
+    private int[] passed;
+    MyGraph(int len) {
+        this.len = len;
+        list = new ArrayList<>();
+        for(int i=0; i<=len; i++)
+            list.add(new HashMap<>());
+    }
+    public void add(int i, int j, int time) {
+        HashMap<Integer, Integer> iMap = list.get(i);
+        if(iMap.containsKey(j) && iMap.get(j) <= time) return;
+
+        iMap.put(j, time);
+        list.get(j).put(i, time);
+    }
+    public int countDelivery(int time) {
+        timeLimit = time;
+        passed = new int[len+1];
+        heap = new PriorityQueue();
+        heap.add(new AccNode(1,0));
+        int result = bfs();
+        return result;    // 자기자신
+    }
+    private int bfs() {
+        if(heap.size() == 0) return 0;
+        int count = 0;
+        PriorityQueue<AccNode> nextHeap = new PriorityQueue<>();
+        while(heap.size() > 0) {
+            AccNode one = heap.poll();
+            int idx = one.idx, curTime = one.accTime;
+            if(curTime > timeLimit) continue;
+            if(passed[idx] == 1) continue;
+            passed[idx] = 1;
+            count += 1;
+            Map<Integer, Integer> idxMap = list.get(idx);
+            for(int nextIdx : idxMap.keySet()) {
+                nextHeap.add(new AccNode(nextIdx, curTime+idxMap.get(nextIdx)));
+            }
+        }
+
+        heap.clear();
+        heap = nextHeap;
+        count += bfs();
+        return count;
+    }
+}
+
+class AccNode implements Comparable<AccNode> {
+    int idx, accTime;
+    AccNode(int i, int time) {
+        idx = i;
+        accTime = time;
+    }
+    @Override
+    public String toString() {
+        return String.format("[idx:%d, accTime:%d]", idx, accTime);
+    }
+    @Override
+    public int compareTo(AccNode accNode) {
+        return accTime - accNode.accTime;
+    }
+
+}
---
+++ algorithm/problems/src/programmers/delivery/SolutionTest.java
@@ -0,0 +1,32 @@
+package programmers.delivery;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void solution() {
+        Solution sol = new Solution();
+        int[][] arr1 = new int[][]{{1,2,1},{2,3,3},{5,2,2},{1,4,2},{5,3,1},{5,4,2}};
+        assertEquals( sol.solution(5, arr1, 3), 4);
+        assertEquals(sol.solution(5, arr1, 4), 5);
+        assertEquals(sol.solution(5, arr1, 1), 2);
+        assertEquals(sol.solution(5, arr1, 0), 1);
+
+        int[][] arr2 = new int[][] {{1, 2, 1},{1, 3, 2}, {2, 3, 2}, {3, 4, 3}, {3, 5, 2}, {3, 5, 3}, {5, 6, 1}};
+        assertEquals(sol.solution(6, arr2, 4), 4);
+        assertEquals(sol.solution(6, arr2, 5), 6);
+        assertEquals(sol.solution(6, arr2, 2), 3);
+        assertEquals(sol.solution(6, arr2, 1), 2);
+        assertEquals(sol.solution(6, arr2, 0), 1);
+
+        int[][] arr3 = new int[][] {{1,2,2},{1,2,3},{1,2,1},{1,3,3},{1,3,2},{2,4,4},{2,5,4},{2,5,1},{3,4,3},{4,5,2},{4,5,1}};
+        assertEquals(sol.solution(5, arr3, 3), 5);
+        assertEquals(sol.solution(5, arr3, 2), 4);
+
+        int[][] arr4 = new int[][] {{1,2,3},{1,3,1},{2,3,1},{2,4,1}};
+        assertEquals(sol.solution(4, arr4, 3), 4);
+    }
+}
