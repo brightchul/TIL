6131f7613d14e48bbb42994d012718e5e52fcbf6
brightchul <abhbtbb@gmail.com>
2020-03-02 06:49:19 +0900

    [add] 프로그래머스 배달 문제 풀이

--- algorithm/problems/src/programmers/delivery/Solution.java
+++ algorithm/problems/src/programmers/delivery/Solution.java
@@ -1,87 +1,85 @@
 package programmers.delivery;
-
-import java.util.*;
-
 /*
 코딩테스트 연습 < 서머코딩/윈터코딩(~2018) < 배달
 https://programmers.co.kr/learn/courses/30/lessons/12978
  */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int solution(int N, int[][] road, int K) {
-        MyGraph mg = new MyGraph(N);
+        AdjList adjList = new AdjList(N);
         for(int[] one : road)
-            mg.add(one[0], one[1], one[2]);
-        int result = mg.countDelivery(K);
-        return result;
-    }
-}
+            adjList.add(one[0], one[1], one[2]);
 
-
-class MyGraph {
-    private PriorityQueue<AccNode> heap;
-    private List<HashMap<Integer, Integer>> list;
-    private int len;
-    private int timeLimit;
-    private int[] passed;
-    MyGraph(int len) {
-        this.len = len;
-        list = new ArrayList<>();
-        for(int i=0; i<=len; i++)
-            list.add(new HashMap<>());
-    }
-    public void add(int i, int j, int time) {
-        HashMap<Integer, Integer> iMap = list.get(i);
-        if(iMap.containsKey(j) && iMap.get(j) <= time) return;
-
-        iMap.put(j, time);
-        list.get(j).put(i, time);
-    }
-    public int countDelivery(int time) {
-        timeLimit = time;
-        passed = new int[len+1];
-        heap = new PriorityQueue();
-        heap.add(new AccNode(1,0));
-        int result = bfs();
-        return result;    // 자기자신
-    }
-    private int bfs() {
-        if(heap.size() == 0) return 0;
         int count = 0;
-        PriorityQueue<AccNode> nextHeap = new PriorityQueue<>();
-        while(heap.size() > 0) {
-            AccNode one = heap.poll();
-            int idx = one.idx, curTime = one.accTime;
-            if(curTime > timeLimit) continue;
-            if(passed[idx] == 1) continue;
-            passed[idx] = 1;
-            count += 1;
-            Map<Integer, Integer> idxMap = list.get(idx);
-            for(int nextIdx : idxMap.keySet()) {
-                nextHeap.add(new AccNode(nextIdx, curTime+idxMap.get(nextIdx)));
-            }
+        for(int one : adjList.dijkstra(N, 1)) {
+            if(one <= K) count++;
         }
-
-        heap.clear();
-        heap = nextHeap;
-        count += bfs();
         return count;
     }
 }
 
-class AccNode implements Comparable<AccNode> {
-    int idx, accTime;
-    AccNode(int i, int time) {
-        idx = i;
-        accTime = time;
+class AdjList {
+    List<List<MyPair>> list;
+    AdjList(int len) {
+        list = new ArrayList<>(++len);  // 인덱스 1부터 사용
+        while(len-- > 0) list.add(new ArrayList<>());
+    }
+    public void add(int idx1, int idx2, int cost) {
+        list.get(idx1).add(new MyPair(idx2, cost));
+        list.get(idx2).add(new MyPair(idx1, cost));
+    }
+    public int[] dijkstra (int totalCount, int src) {
+        PriorityQueue<MyPair> pq = new PriorityQueue<>();
+        int[] dist = new int[totalCount+1];     // 인덱스 1부터 사용
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        pq.add(new MyPair(src, 0));
+
+        while(pq.size() > 0) {
+            MyPair curPair = pq.poll();
+            int curIdx = curPair.idx;
+            int curCost = curPair.cost;
+            List<MyPair> curList = list.get(curIdx);
+
+            for(int i=0; i<curList.size(); i++) {
+                MyPair one = curList.get(i);
+                int nextIdx = one.idx;
+                int nextCost = curCost + one.cost;
+
+                if(dist[nextIdx] <= nextCost) continue;
+
+                if(dist[nextIdx] > nextCost) {
+                    dist[nextIdx] = nextCost;
+                    pq.add(new MyPair(nextIdx, nextCost));
+                }
+            }
+        }
+        return arrayCopy(dist, 1, totalCount);
+    }
+    public static int[] arrayCopy(int[] oriArr, int oriIdx, int copyLength) {
+        int[] retArr = new int[copyLength];
+        System.arraycopy(oriArr, oriIdx, retArr, 0, copyLength);
+        return retArr;
+    }
+}
+
+class MyPair implements Comparable<MyPair>{
+    int idx, cost;
+    MyPair(int idx, int cost) {
+        this.idx = idx;
+        this.cost = cost;
+    }
+    @Override
+    public int compareTo(MyPair myPair) {
+        return cost - myPair.cost;
     }
     @Override
     public String toString() {
-        return String.format("[idx:%d, accTime:%d]", idx, accTime);
+        return String.format("[idx:%s, cost:%s]", idx, cost);
     }
-    @Override
-    public int compareTo(AccNode accNode) {
-        return accTime - accNode.accTime;
-    }
-
 }
