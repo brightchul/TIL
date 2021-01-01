72a980655aa6d785a30c1083ae1606c6d1538770
brightchul <abhbtbb@gmail.com>
2021-01-01 23:59:28 +0900

    [add] test case 추가 (jest)

---
+++ algorithm/dynamicProgramming/chapter5/src/__tests__/interleaving.test.ts
@@ -0,0 +1,10 @@
+import { isInterleavingDP, isInterleavingRecursive } from "../interleaving";
+
+test("isInterleavingRecursive", () => {
+  expect(isInterleavingRecursive("bcc", "bbca", "bbcbcac")).toBeTruthy();
+  expect(isInterleavingRecursive("bca", "bbca", "bbcbcac")).toBeFalsy();
+});
+
+test("isInterleavingDP", () => {
+  expect(isInterleavingDP("bcc", "bbca", "bbcbcac")).toBeTruthy();
+});
